747aa1738e25be4e3004594a60a11208fc77c5f9
Zane Stock <zanestock3@gmail.com>
2022-05-12 11:46:06 -0500

    commit tree 29e4dd26d108eca24e5e7ab8803951fe13de8c81

---
+++ index.js
@@ -0,0 +1,22 @@
+let divide = (a, b) => {
+    return a / b;
+}
+divide = (a = 2000, b = 100) => {
+    return a / b
+}
+
+let square = (c, d) => {
+    return (c * d)
+}
+
+square = (c = 10) => {
+    return (c * c)
+}
+
+let add = (e, f) => {
+    return e + f
+}
+
+add = (e, f) => {
+    return e + f
+}
